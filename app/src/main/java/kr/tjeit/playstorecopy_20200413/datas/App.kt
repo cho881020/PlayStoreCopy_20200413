9e6e933ba22539a5a77437887ddc36faa74eef54
cho881020 <kj_cho@idealidea.co.kr>
2020-04-13 21:13:09 +0900

    데이터클래스 생성

---
+++ app/src/main/java/kr/tjeit/playstorecopy_20200413/datas/App.kt
@@ -0,0 +1,7 @@
+package kr.tjeit.playstorecopy_20200413.datas
+
+class App(name:String, companyName:String, starCount:Int) {
+    var name = name
+    var companyName = companyName
+    var starCount = starCount
+}
